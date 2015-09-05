a57d74c4230adf3b8555ff798021655bb645dc21
Ethan <ethanabrooks@gmail.com>
2015-09-05 15:54:46 -0400

    working on inserting busytimes in db

--- app.py
+++ app.py
@@ -4,12 +4,38 @@ import httplib2
 
 from apiclient import discovery
 from oauth2client import client
-from flask import Flask, session, redirect, render_template, url_for, request
+from flask_restful import Resource, Api, reqparse
+from flask import Flask, session, redirect, render_template, url_for, request, json
 
 app = Flask(__name__)
+api = Api(app)
+
+class CreateBusyTime(Resource):
+    def post(self): 
+        try:
+            # Parse the arguments 
+            api.add_resource(CreateBusyTime, '/CreateBusyTime')
+
+            Parser = reqparse.RequestParser() 
+            parser.add_argument('timeMin', type=str, help='start of busy time') 
+            parser.add_argument('timeMax', type=str, help='end of busy time') 
+            args = parser.parse_args()
+
+            _timeMin = args['timeMin']
+            _timeMax = args['timeMax']
+
+            return {'timeMin': args['timeMin'], 'timeMax': args['timeMax']}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+api.add_resource(CreateBusyTime, '/CreateBusyTime')
 
 @app.route('/')
 def index():
+    now = datetime.datetime.now()
+    timeMin = now.isoformat() + 'Z'  # 'Z' indicates UTC timenow
+    timeMax = (now + datetime.timedelta(days=5, hours=3)).isoformat() + 'Z'
     if 'credentials' not in session:
 
         # requests access from user
@@ -22,22 +48,46 @@ def index():
 
         # "service" will perform all the basic functions of the Calendar API
         service = discovery.build('calendar', 'v3', http_auth)
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
         print('Getting the upcoming 10 events') 
 
-        # eventsResult requests the next 10 events in the user's calendar
+        # eventsResult requests events in the user's calendar between 
+        # timeMin and timeMax
+        calendarId='0mninvkc7gf8fajkq991mo93to@group.calendar.google.com' # change this to 'primary' in final version
         eventsResult = service.events().list(
-            calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+            calendarId=calendarId,
+            timeMin=timeMin,
+            timeMax=timeMax,
+            singleEvents=True,
             orderBy='startTime').execute()
 
     # events actually retrieves those 10 events
     events = eventsResult.get('items', [])
-
+    busyTimes = [(event['start']['dateTime'],event['end']['dateTime']) for event in events]
+    
     if not events:
         return 'No upcoming events found.'
 
     # renders index.html in the browser and provides the 'events' variable to it
-    return render_template('index.html', events=events)
+    return render_template('index.html', busyTimes=busyTimes)
+
+@app.route('/showSignUp')
+def showSignUp():
+    return "hi"
+    return render_template('signup.html')
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 @app.route('/oauth2callback')
 def oauth2callback():
@@ -62,13 +112,9 @@ def oauth2callback():
         session['credentials'] = credentials.to_json()
         return redirect(url_for('index'))
 
-@app.route('/showSignUp')
-def showSignUp():
-    return render_template('signup.html')
-
 if __name__ == '__main__':
     import uuid
 
     app.secret_key = str(uuid.uuid4())
     app.debug = True
-    app.run()
+    app.run(port=8000)
